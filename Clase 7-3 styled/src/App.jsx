7e963a56c05e520f14bf1eeb56f9acce026a63b7
KenyiTeranKomarcaLabs <kenyiterankomarcalabs@gmail.com>
2024-06-28 11:47:15 -0500

    Add files via upload

---
+++ Clase 7-3 styled/src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  color: red;
+`;
+
+function App() {
+  return (
+    <>
+      <MyButton>Hola mundo</MyButton>
+    </>
+  );
+}
+export default App;
